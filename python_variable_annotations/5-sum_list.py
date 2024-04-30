e5d9ab1d31f0368b50a88755836696f3d363bf81
jordiindahood <hazem.ouni123@gmail.com>
2024-05-01 00:17:31 +0100

    script 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    script 3
+"""
+from typing import List
+
+
+def sum_list(mylist: List[float]) -> float:
+    """
+    a function that returns the floor of n
+    """
+    return sum(mylist)
