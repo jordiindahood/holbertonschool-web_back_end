6329fa13684f3fd98404122906a9785f06f2f167
jordiindahood <hazem.ouni123@gmail.com>
2024-05-01 00:56:34 +0100

    script 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    script 3
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    a function that returns the floor of n
+    """
+    return sum(mxd_lst)
