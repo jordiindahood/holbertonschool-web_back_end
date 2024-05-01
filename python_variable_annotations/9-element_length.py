17192293470021433c39722a2616f7a8393568b6
jordiindahood <hazem.ouni123@gmail.com>
2024-05-01 18:36:23 +0100

    script 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,8 +7,7 @@ from typing import List, Tuple, Sequence, Iterable
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    a function that returns a list of tuple
-    Return: list[tuple[sequence]]
+    a function that returns a list of tuple Return: list[tuple[sequence]]
     """
 
     return [(i, len(i)) for i in lst]
