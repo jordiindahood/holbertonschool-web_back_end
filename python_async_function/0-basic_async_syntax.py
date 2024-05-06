3083ca1f884b9131af33c5a4e4826f5a85be5158
jordiindahood <hazem.ouni123@gmail.com>
2024-05-06 23:10:45 +0100

    script 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+script 0
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    function that takes an integer argument
+    max_delay and returns a asyncio.Task
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
