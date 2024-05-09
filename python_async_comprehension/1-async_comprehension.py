8b092f1b6b2e5476e16855d9709db6e9a1362131
jordiindahood <hazem.ouni123@gmail.com>
2024-05-09 13:14:33 +0100

    docstring correction

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,7 +10,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """Function that return a list"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
+    result = [result async for result in async_generator()]
     return result
