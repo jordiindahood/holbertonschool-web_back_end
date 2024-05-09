5b7a908459427019223d6a34c80be34c9d39d747
jordiindahood <hazem.ouni123@gmail.com>
2024-05-09 13:10:16 +0100

    script 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' Function that return a list '''
+import asyncio
+from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Function that return a list"""
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
