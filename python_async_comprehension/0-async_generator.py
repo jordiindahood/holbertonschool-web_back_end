769996dcf8afb1f462cb160ca99bc307180e35b3
jordiindahood <hazem.ouni123@gmail.com>
2024-05-09 12:24:59 +0100

    pycodestyle correction

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """
     function that returns a list of
     random numbers between 0 and 10
